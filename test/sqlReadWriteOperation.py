c57213486220ccdaa52c47164367cb7574878cef
Creativitylevel0 <parihargaurav1999@gmail.com>
2026-01-21 11:04:43 +0530

    Created Function to read and write data from so I don't have to provide details of db again and again

--- test/sqlReadWriteOperation.py
+++ test/sqlReadWriteOperation.py
@@ -6,6 +6,47 @@ sys.path.append(str(PROJECT_ROOT))
 from pyspark.sql import SparkSession
 from dbConfig import get_db_config
 
+def getDataframeFromCSVFileWithHeader(sparksession, filePath, fileSchema = None, filedelim=","):
+    reader = sparksession.read \
+                .option("header", "true") \
+                .option("sep", filedelim) \
+                .option("nullValue", "null")
+
+    # If a schema is provided, use it
+    if fileSchema:
+        reader = reader.schema(fileSchema)
+    else:
+        # Infer schema automatically
+        reader = reader.option("inferSchema", "true")
+    
+    outputDF = reader.csv(filePath)
+    return outputDF
+
+def read_from_db(sparkSession, table_name, jdbc_url):
+   df = sparkSession.read \
+        .format("jdbc") \
+        .option("driver", db['driver']) \
+        .option("url", jdbc_url) \
+        .option("dbtable", table_name) \
+        .option("user", db['user']) \
+        .option("password", db['password']) \
+        .option("encrypt", db['encrypt']) \
+        .option("trustServerCertificate", db['trust_cert']) \
+        .load()
+   return df
+
+def write_to_db(df, table_name, jdbc_url):
+        df.write \
+        .format("jdbc") \
+        .mode("overwrite") \
+        .option("url", jdbc_url) \
+        .option("driver", db['driver']) \
+        .option("dbtable", table_name) \
+        .option("user", db['user']) \
+        .option("password", db['password']) \
+        .option("encrypt", db['encrypt']) \
+        .option("trustServerCertificate", db['trust_cert']) \
+        .save()
 
 if __name__ == '__main__':
     spark = SparkSession.builder \
@@ -23,7 +64,7 @@ if __name__ == '__main__':
     logFileSchema = ("""date DATE,
                         time TIMESTAMP,
                         size INT,
-                        r_version FLOAT,
+                        r_version STRING,
                         r_arch STRING,
                         r_os STRING,package STRING,
                         version STRING,
@@ -35,8 +76,16 @@ if __name__ == '__main__':
                 .schema(logFileSchema)
                 .csv(logFileLoc)
 )
-    
-    # jdbcDF = spark.read \
+    orderFileLoc = "D:\\pyspark-training\\data\\mongodb-classicmodels-master\\csv\\order.csv"
+    ordersSchema = "orderNumber int,orderDate date,requiredDate date,shippedDate date,status string,comments string,customerNumber int"
+    ordersDF = getDataframeFromCSVFileWithHeader(spark, orderFileLoc, ordersSchema)
+    write_to_db(ordersDF, "dbo.ordersTable", jdbc_url)
+    orderTableFromDb = read_from_db(spark, "dbo.ordersTable", jdbc_url)
+    orderTableFromDb.show(5)
+    logFileFromDb = read_from_db(spark, "dbo.logTable", jdbc_url)
+    logFileFromDb.show(5)
+
+        # jdbcDF = spark.read \
     #     .format("jdbc") \
     #     .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
     #     .option("url", "jdbc:sqlserver://localhost:1433;databaseName=GauravDb") \
@@ -52,25 +101,23 @@ if __name__ == '__main__':
     #     .mode("overwrite") \
     #     .option("url", "jdbc:sqlserver://localhost:1433;databaseName=GauravDb") \
     #     .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
-    #     .option("dbtable", "logTable") \
+    #     .option("dbtable", "dbo.logTable") \
     #     .option("user", "pyspark") \
     #     .option("password", "pyspark") \
     #     .option("encrypt", "true") \
     #     .option("trustServerCertificate", "true") \
     #     .save()
     
-    logFileFromDb = spark.read \
-        .format("jdbc") \
-        .option("driver", "com.microsoft.sqlserver.jdbc.SQLServerDriver") \
-        .option("url", jdbc_url) \
-        .option("dbtable", "dbo.logTable") \
-        .option("user", db['user']) \
-        .option("password", db['password']) \
-        .option("encrypt", db['encrypt']) \
-        .option("trustServerCertificate", db['trust_cert']) \
-        .load()
-    
-    logFileFromDb.show(5)
+    # logFileFromDb = spark.read \
+    #     .format("jdbc") \
+    #     .option("driver", db['driver']) \
+    #     .option("url", jdbc_url) \
+    #     .option("dbtable", "dbo.logTable") \
+    #     .option("user", db['user']) \
+    #     .option("password", db['password']) \
+    #     .option("encrypt", db['encrypt']) \
+    #     .option("trustServerCertificate", db['trust_cert']) \
+    #     .load()
 
     # logFileDF.show(5)
 
